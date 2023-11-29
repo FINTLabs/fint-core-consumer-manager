a5c8a6df27e6ec644385dbb45420d2fda2ea59fc
Hknots <Hknots99@gmail.com>
2023-11-29 15:04:42 +0100

    initial commit

---
+++ src/test/kotlin/no/fintlabs/consumer/manager/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.fintlabs.consumer.manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
