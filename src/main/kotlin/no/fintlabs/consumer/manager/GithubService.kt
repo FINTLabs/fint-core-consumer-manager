a0592db4807a897621fab392630f1c24ebf23ffe
Hknots <Hknots99@gmail.com>
2023-12-01 12:54:28 +0100

    add github service

---
+++ src/main/kotlin/no/fintlabs/consumer/manager/GithubService.kt
@@ -0,0 +1,40 @@
+package no.fintlabs.consumer.manager
+
+import org.kohsuke.github.GHRepository
+import org.kohsuke.github.GitHub
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+@Service
+class GithubService {
+
+    @Value("\${fint.github.token:")
+    private lateinit var token: String
+
+    @Value("\${fint.github.username:FINTLabs}")
+    private lateinit var username: String
+
+    private val github: GitHub = GitHub.connectUsingOAuth(token)
+
+    fun updateVersion(repo: String, version: String) {
+        val repository: GHRepository = github.getRepository("$username/$repo")
+        createBranch(repository)
+        // Update version in gradle.properties
+        // Commit and push to github
+        // Create pull request
+    }
+
+    private fun createBranch(repo: GHRepository) {
+        // Create branch
+        // Checkout branch
+    }
+
+    private fun updateBuildGradle(repo: String, branch: String, version: String) {
+        // Update version in build.gradle
+    }
+
+    private fun createPullRequest(repo: String, branch: String) {
+        // Create pull request
+    }
+
+}
