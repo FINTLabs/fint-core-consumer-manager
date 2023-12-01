ca9beb1b36216c618e19cbb9343f15f96fe25c44
Hknots <Hknots99@gmail.com>
2023-12-01 13:28:24 +0100

    add function to update spring boot version

--- src/main/kotlin/no/fintlabs/consumer/manager/GithubService.kt
+++ src/main/kotlin/no/fintlabs/consumer/manager/GithubService.kt
@@ -4,6 +4,8 @@ import org.kohsuke.github.GHRepository
 import org.kohsuke.github.GitHub
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
+import java.io.BufferedReader
+import java.nio.charset.StandardCharsets
 
 @Service
 class GithubService {
@@ -18,18 +20,26 @@ class GithubService {
 
     fun updateVersion(repo: String, version: String) {
         val repository: GHRepository = github.getRepository("$username/$repo")
-        createBranch(repository)
-        // Update version in gradle.properties
-        // Commit and push to github
-        // Create pull request
+        val branchName = "update-spring-$version"
+        createBranch(repository, branchName)
+        updateBuildGradle(repository, branchName, version)
+        createPullRequest(repository, branchName)
     }
 
     private fun createBranch(repo: GHRepository, branchName: String) {
         repo.createRef("refs/heads/$branchName", repo.getRef("heads/main").getObject().sha)
     }
 
-    private fun updateBuildGradle(repo: String, branch: String, version: String) {
-        // Update version in build.gradle
+    private fun updateBuildGradle(repo: GHRepository, branch: String, version: String) {
+        val content = repo.getFileContent("build.gradle", branch)
+        val inputStream = content.read()
+
+        val oldContent = inputStream.bufferedReader(StandardCharsets.UTF_8).use(BufferedReader::readText)
+        val versionRegex = "id ['\"]org.springframework.boot['\"] version ['\"].*['\"]".toRegex()
+        val newVersionString = "id 'org.springframework.boot' version '$version'"
+        val updatedContent = versionRegex.replace(oldContent, newVersionString)
+
+        content.update(updatedContent, "Update Spring Boot version to $version", branch)
     }
 
     private fun createPullRequest(repo: GHRepository, branch: String) {
