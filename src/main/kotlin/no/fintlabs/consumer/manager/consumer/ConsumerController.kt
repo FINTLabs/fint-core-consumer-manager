f09d3c4fd21ee7f19cc040a3a1cc5d1524eee3d1
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2023-12-07 10:55:55 +0100

    sping version endpoint

--- src/main/kotlin/no/fintlabs/consumer/manager/consumer/ConsumerController.kt
+++ src/main/kotlin/no/fintlabs/consumer/manager/consumer/ConsumerController.kt
@@ -2,7 +2,6 @@ package no.fintlabs.consumer.manager.consumer
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,14 +11,9 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/consumer")
 class ConsumerController(val consumerService: ConsumerService, val consumerUpdateService: ConsumerUpdateService) {
 
-    @GetMapping("/{repoName}")
-    fun getRepo(@PathVariable repoName: String): Map<String, String> {
-        return consumerService.getSpringVersion(repoName)
-    }
-
-    @GetMapping("topic")
-    fun getTopic(): List<String>{
-        return consumerService.getConsumerRepos()
+    @GetMapping("/")
+    fun getRepo(): Map<String, String> {
+        return consumerService.getSpringVersion()
     }
 
     @PostMapping("/update/spring")
--- src/main/kotlin/no/fintlabs/consumer/manager/consumer/ConsumerService.kt
+++ src/main/kotlin/no/fintlabs/consumer/manager/consumer/ConsumerService.kt
@@ -12,18 +12,22 @@ class ConsumerService {
         private val log = LoggerFactory.getLogger(ConsumerService::class.java)
     }
 
-    fun getSpringVersion(repoName: String): Map<String, String> {
+    fun getSpringVersion(): Map<String, String> {
         val map = mutableMapOf<String, String>()
+        val repos: List<String> = getConsumerRepos();
+        for (repo in repos){
         try {
             val restTemplate = RestTemplate()
-            val result = restTemplate.getForObject("https://raw.githubusercontent.com/FINTLabs/$repoName/main/build.gradle", String::class.java)
+            val result = restTemplate.getForObject(
+                "https://raw.githubusercontent.com/FINTLabs/${repo}/main/build.gradle",
+                String::class.java
+            )
 
             result?.let {
                 if (it.contains("org.springframework.boot' version")) {
                     it.split("\n").forEach { line ->
                         if (line.contains("org.springframework.boot' version")) {
-                            map[repoName] = line.trim().replace("id 'org.springframework.boot' version", "")
-                            return map
+                            map[repo] = line.trim().replace("id 'org.springframework.boot' version", "")
                         }
                     }
                 }
@@ -31,9 +35,9 @@ class ConsumerService {
         } catch (e: Exception) {
             log.info("error:", e)
         }
+        }
         return map
     }
-
 
     val repos = mutableListOf<String>()
     fun getConsumerRepos(): List<String> {
@@ -52,8 +56,6 @@ class ConsumerService {
         } catch (e: Exception) {
             log.error("Error:", e)
         }
-
         return emptyList()
     }
 }
-
