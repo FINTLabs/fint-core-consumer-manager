c168538f918534691ae793f83e46f19d7d148a4f
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-02-23 14:58:37 +0100

    add method to fint gradle version in projects && rename method to find latest gradle version

--- src/main/kotlin/no/fintlabs/consumer/manager/gradle/GradleController.kt
+++ src/main/kotlin/no/fintlabs/consumer/manager/gradle/GradleController.kt
@@ -2,6 +2,7 @@ package no.fintlabs.consumer.manager.gradle
 
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,9 +10,13 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/gradle")
 class GradleController(val gradleService: GradleService) {
 
-    @GetMapping("/version")
-    fun getGradleVersion(): ResponseEntity<String> {
-        return ResponseEntity.ok(gradleService.getGradleVersion())
+    @GetMapping("/{repo}")
+    fun getGradleVersions(@PathVariable repo: String): ResponseEntity<Map<String, String>> {
+        return ResponseEntity.ok(gradleService.getGradleVersions(repo))
     }
 
+    @GetMapping("/version")
+    fun getLatestGradleVersion(): ResponseEntity<String> {
+        return ResponseEntity.ok(gradleService.getLatestGradleVersion())
+    }
 }
--- src/main/kotlin/no/fintlabs/consumer/manager/gradle/GradleService.kt
+++ src/main/kotlin/no/fintlabs/consumer/manager/gradle/GradleService.kt
@@ -1,9 +1,11 @@
 package no.fintlabs.consumer.manager.gradle
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import no.fintlabs.consumer.manager.consumer.ConsumerService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
+import kotlin.math.log
 
 @Service
 class GradleService {
@@ -13,10 +15,32 @@ class GradleService {
 
     private val restTemplate: RestTemplate = RestTemplate()
 
-    fun getGradleVersion(): String {
+    fun getLatestGradleVersion(): String {
         val response = restTemplate.getForObject(baseUrl, String::class.java)
         val jsonNode = jacksonObjectMapper().readTree(response)
         return jsonNode.path("version").asText()
     }
 
+    fun getGradleVersions(repo:String): Map<String, String> {
+        val map = mutableMapOf<String, String>()
+        try {
+            val restTemplate = RestTemplate()
+            val result = restTemplate.getForObject(
+                "https://raw.githubusercontent.com/FINTLabs/${repo}" +
+                        "/main/gradle/wrapper/gradle-wrapper.properties", String::class.java
+            )
+            result.let {
+                result?.split("\n")?.forEach {line ->
+                    if (line.contains("distributionUrl")) {
+                        val version = line.subSequence(66, 69).toString()
+                        map.put(repo, version)
+                    }
+                }
+            }
+            return map
+        } catch (e: Exception) {
+            print(e)
+        }
+        return emptyMap();
+    }
 }
